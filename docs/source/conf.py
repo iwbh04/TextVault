5d4596a4fa7a714ab43934bdbd6e9ab4cb9f6f45
OSH <ohsanghoon0422@gmail.com>
2024-12-04 14:34:04 +0900

    A5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import sys
 from pathlib import Path
-sys.path.insert(0, str(Path(__file__).resolve().parents[20]))
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
 project = 'TEXTVAULT_LIBRARY'
 copyright = '2024, SANGHOON OH'
