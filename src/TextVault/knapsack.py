cec2ed8cd3496afd3dd6dad509a361d5158900c7
Bumsoo Kim <bumsoo0515@gmail.com>
2024-12-06 17:17:19 +0900

    Update knapsack.py

--- src/TextVault/knapsack.py
+++ src/TextVault/knapsack.py
@@ -3,37 +3,61 @@ import random
 import base64
 
 class KnapsackEncryptor(Encryptor):
-    # 설명 작성 필요
-
+    """
+    Encrypt text with https://en.wikipedia.org/wiki/Merkle%E2%80%93Hellman_knapsack_cryptosystem.
+    """
+    
     @staticmethod
-    def __encode(li: list[int]) -> str:
+    def __list_encode(li: list[int]) -> str:
         b = b','.join(base64.b64encode(i.to_bytes(16)) for i in li)
         return b.decode()
 
     @staticmethod
-    def __decode(txt: str) -> list[int]:
+    def __list_decode(txt: str) -> list[int]:
         ret = [int.from_bytes(base64.b64decode(i)) for i in txt.split(',')]
         return ret
     
     def encrypt(self, text: str, key: Key) -> str:
+        """
+        Encrypt text with public key and return result.
+
+        :param text: Text to encrypt.
+        :type text: str
+        :param key: public key
+        :type key: TextVault.Key
+        :return: encrypted text.
+        :rtype: str
+
+        """
         if key.encryptor != self.__class__: raise KeyTypeNotMatch("Encryptor doesn't match")
-        key = self.__decode(key.value)
+        key = self.__list_decode(key.value)
 
         # 임시로 각 값의 ord를 사용
         binary_message = [f'{ord(c):0{len(key)}b}' for c in text]
 
         encrypted = [sum(int(x[i])*key[i] for i in range(len(key))) for x in binary_message]
-        return self.__encode(encrypted)
+        return self.__list_encode(encrypted)
     
     def decrypt(self, text: str, key: Key) -> str:
+        """
+        Decrypt text with private key and return result.
+
+        :param text: Text to decrypt.
+        :type text: str
+        :param key: private key
+        :type key: TextVault.Key
+        :return: decrypted text.
+        :rtype: str
+
+        """
         if key.encryptor != self.__class__: raise KeyTypeNotMatch("Encryptor doesn't match")
-        key = self.__decode(key.value)
+        key = self.__list_decode(key.value)
         if len(key) < 3: raise KeyTypeNotMatch("Key format doesn't match")
 
         *arr, mod, mult = key
         inv_mult = pow(mult, -1, mod)
         
-        encrypted = self.__decode(text)
+        encrypted = self.__list_decode(text)
         encrypted = [i*inv_mult%mod for i in encrypted]
 
         recovered = []
@@ -49,7 +73,15 @@ class KnapsackEncryptor(Encryptor):
 
         return ''.join(chr(i) for i in recovered)
     
-    def newkey(self) -> Key | tuple[Key]:
+    def newkey(self) -> tuple[Key]:
+        """
+        Return a new (public key, private key) tuple for knapsack-cryptosystem.
+
+        :return: new (public key, private key) tuple
+        :rtype: tuple[Key]
+
+        """
+
         length = 10
 
         def isprime(n):
@@ -79,7 +111,7 @@ class KnapsackEncryptor(Encryptor):
         public_key = [(x * mult) % mod for x in private_key]
         private_key += [mod, mult]
 
-        return Key(self.__encode(public_key), self.__class__), Key(self.__encode(private_key), self.__class__)
+        return Key(self.__list_encode(public_key), self.__class__), Key(self.__list_encode(private_key), self.__class__)
 
 """
 enc = KnapsackEncryptor()
