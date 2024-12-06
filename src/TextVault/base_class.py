f50fe4f93169a780870e85eb8cf5d7e616acd740
아시라프 <asyraf67@hanyang.ac.kr>
2024-12-06 14:30:53 +0900

    Create base_class.py

---
+++ src/TextVault/base_class.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+class Key:
+    def __init__(self, value, encryptor):
+        self.value: str = value
+        self.encryptor: Encryptor = encryptor
+    
+    def export(self, file_name: str):
+        pass
+
+
+class Encryptor(ABC):
+    @abstractmethod
+    def newkey(self) -> Key | tuple[Key]:
+        pass
+    
+    @abstractmethod
+    def encrypt(self, text: str, key: Key) -> str:
+        pass
+    
+    @abstractmethod
+    def decrypt(self, text: str, key: Key) -> str:
+        pass
+
+# class LengthLimitExceed(Exception):
+#     # 암호화 가능한 문자열의 길이를 초과한 경우
+#     pass
+
+# class KeyTypeNotMatch(Exception):
+#     pass
