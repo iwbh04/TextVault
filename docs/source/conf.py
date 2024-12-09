62b8d57286fd13c601d7ed3a8f678f76af3f2b5d
Bumsoo Kim <82440135+bumsoo0515@users.noreply.github.com>
2024-12-09 10:04:14 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[2]/"src"))
 project = 'TextVault'
 copyright = '2024, SANGHOON OH'
 author = 'SANGHOON OH'
-release = '0.0.2'
+release = '0.0.3'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
